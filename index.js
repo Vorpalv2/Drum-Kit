aa79dbd88274ceca5652373756f3903a5c844c55
Aakash <aakashanand1995@gmail.com>
2023-04-10 19:40:02 +0530

    event listener

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("looks to be working");
+
+var loopender = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < loopender; i++) {
+   document.querySelectorAll(".drum")[i].addEventListener("click",eventPressed);
+}
+
+
+function eventPressed(){
+    alert("I got pressed");
+}
