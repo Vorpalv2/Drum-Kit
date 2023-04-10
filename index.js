ed7930bf4bd2cde0ce3b0c66ff260130dfbe63ab
Aakash <aakashanand1995@gmail.com>
2023-04-10 23:22:59 +0530

    Update index.js
    Higher Order Function and Delegates with passing functions as arguments

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ for (var i = 0; i < loopender; i++) {
 function eventPressed(){
     alert("I got pressed");
 }
+
+
+function add(num1,num2){
+    return num1+num2;
+}
+function subtract(num1,num2){
+    return num1-num2;
+
+}
+function multiply(num1,num2){
+    return num1*num2;
+}
+function divide(num1,num2){
+    return num1/num2;
+}
+function modulo(num1,num2){
+    return num1%num2;
+}
+
+function calculator(num1,num2,operator){
+    return operator(num1,num2);
+}
