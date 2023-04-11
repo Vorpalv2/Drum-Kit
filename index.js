133f78006875bf3fef548cc6461107f65a2cb909
Aakash <aakashanand1995@gmail.com>
2023-04-11 22:00:17 +0530

    commit before major edit

--- index.js
+++ index.js
@@ -3,14 +3,43 @@ var loopender = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < loopender; i++) {
    document.querySelectorAll(".drum")[i].addEventListener("click",eventPressed);
+
+   
+        switch (switched) {
+            case "w":
+             var firstAudio = new Audio("sounds/tom-1.mp3");
+             firstAudio.play();
+             break;
+
+        default:
+        break;
+}
+
 }
 
 
-function eventPressed(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function eventPressed(){
     var audioObject = new Audio("sounds/tom-1.mp3");
     audioObject.play();
-    this.style.color="white";
 }
+*/
+
+
 
 /*
 function add(num1,num2){
@@ -32,5 +61,26 @@ function modulo(num1,num2){
 
 function calculator(num1,num2,operator){
     return operator(num1,num2);
+}
+*/
+
+/*
+function HouseKeeper(name,age,cleaning,workpermit){
+    this.name =name;
+    this.age=age;
+    this.cleaning= cleaning;
+    this.workpermit = workpermit;
+};
+
+var firsthousekeeper = new HouseKeeper("Adamn",46,"Bathroom",true);
+
+// new way to declare an object
+class HouseCleaner{
+    constructor(name,age,cleaning,workpermit){
+        this.name = name;
+        this.age = age;
+        this.workpermit = workpermit;
+        this.cleaning = cleaning;
+    }
 }
 */
