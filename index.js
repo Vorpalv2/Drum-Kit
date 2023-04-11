ad9fc2ad64b067371f4feac6f0f3ab947324800c
Aakash <aakashanand1995@gmail.com>
2023-04-11 22:12:01 +0530

    added the switch statement

--- index.js
+++ index.js
@@ -2,18 +2,48 @@
 var loopender = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < loopender; i++) {
-   document.querySelectorAll(".drum")[i].addEventListener("click",eventPressed);
+   document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+   
+    var innerHTMLswitch = this.innerHTML;
+
+    switch (innerHTMLswitch) {
+        case "w":
+            var firstAudio = new Audio("sounds/tom-1.mp3");
+            firstAudio.play();
+            break;
+
+        case "a":
+            var secondAudio = new Audio("sounds/tom-2.mp3");
+            secondAudio.play();
+            break;
+        
+        case "s":
+            var thirdAudio = new Audio("sounds/tom-3.mp3");
+            thirdAudio.play();
+            break;
+        case "d":
+            var fourthAudio = new Audio("sounds/tom-4.mp3");
+            fourthAudio.play();
+            break;
+        case "j":
+            var fifthAudio = new Audio("sounds/snare.mp3");
+            fifthAudio.play();
+        case "k":
+            var sixthAudio = new Audio("sounds/kick-bass.mp3");
+            sixthAudio.play();
+            break;
+        case "l":
+            var seventhAudio = new Audio("sounds/crash.mp3");
+            seventhAudio.play();
+            break;
+        default:
+            console.log(innerHTMLswitch);
+            break;
+    }
+  });
 
    
-        switch (switched) {
-            case "w":
-             var firstAudio = new Audio("sounds/tom-1.mp3");
-             firstAudio.play();
-             break;
-
-        default:
-        break;
-}
+       
 
 }
 
